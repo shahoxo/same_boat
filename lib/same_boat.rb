caa1489ebf7b70fd02a291806295ea4a2140c374
Jun Sitow <jshitou@aiming-inc.com>
2017-11-01 15:40:49 +0900

    Add rake tasks

--- lib/same_boat.rb
+++ lib/same_boat.rb
@@ -4,7 +4,15 @@ module SameBoat
   autoload :Crews, 'same_boat/crews'
   autoload :Crew, 'same_boat/crew'
 
-  def self.accept(crews)
-    Crews.new(crews)
+  DEFAULT_JOURNAL = '.same_boat.journal'.freeze
+  DEFAULT_CREWS = '.env*'.freeze
+
+  def self.accept(crews, journal_path:)
+    Crews.new(crews, journal_path: journal_path)
+  end
+
+  def self.build(crew_paths:, journal_path:)
+    crews = crew_paths.map { |path| Crew.new(path) }
+    accept(crews, journal_path: journal_path)
   end
 end
