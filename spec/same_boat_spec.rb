e218afec9588ce67fa5201f781015705eaa07000
Masashi AKISUE <makisue@aiming-inc.com>
2017-11-02 16:09:17 +0900

    fix tests

--- spec/same_boat_spec.rb
+++ spec/same_boat_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe SameBoat do
   describe '.accept' do
-    subject { described_class.accept(double('crews')) }
+    subject { described_class.accept(double('crews'), journal_path: SameBoat::DEFAULT_JOURNAL) }
     it { should be_a described_class::Crews }
   end
 end
